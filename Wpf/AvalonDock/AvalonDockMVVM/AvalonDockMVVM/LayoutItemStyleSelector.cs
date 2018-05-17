a6002b707d2f3d08c2e7ec8f4a237d3416a0febb
Sobczyk, Raphael <sobczyk.raphael@siemens.com>
2018-05-17 11:39:09 +0200

    1. Add Style selector (with binding) to document- and anchorable-LayoutItem; 2. Add TemplateSelector (with binding) to document- and anchorable-LayoutItem;

---
+++ Wpf/AvalonDock/AvalonDockMVVM/AvalonDockMVVM/LayoutItemStyleSelector.cs
@@ -0,0 +1,43 @@
+﻿#region Copyright
+
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright company="Siemens AG" file="LayoutItemStyleSelector.cs">
+//   Copyright (C) Siemens AG 2018-2018. All rights reserved. Confidential.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+#endregion
+
+
+#region Used namespaces
+
+using System.Windows;
+using System.Windows.Controls;
+
+using AvalonDockMVVM.ViewModel;
+
+#endregion
+
+
+namespace AvalonDockMVVM
+{
+    public class LayoutItemStyleSelector : StyleSelector
+    {
+
+        public Style DocumentStyle { get; set; }
+
+        public Style AnchorableStyle { get; set; }
+
+        public override Style SelectStyle(object item, DependencyObject container)
+        {
+            if (item is SampleDocumentVm)
+                return DocumentStyle;
+
+            if (item is SampleAnchorableVm)
+                return AnchorableStyle;
+
+            return base.SelectStyle(item, container);
+        }
+
+    }
+}
---
+++ Wpf/AvalonDock/AvalonDockMVVM/AvalonDockMVVM/LayoutItemTemplateSelector.cs
@@ -0,0 +1,43 @@
+﻿#region Copyright
+
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright company="Siemens AG" file="LayoutItemTemplateSelector.cs">
+//   Copyright (C) Siemens AG 2018-2018. All rights reserved. Confidential.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+#endregion
+
+
+#region Used namespaces
+
+using System.Windows;
+using System.Windows.Controls;
+
+using AvalonDockMVVM.ViewModel;
+
+#endregion
+
+
+namespace AvalonDockMVVM
+{
+    public class LayoutItemTemplateSelector : DataTemplateSelector
+    {
+
+        public DataTemplate DocumentLayoutItemContentTemplate { get; set; }
+
+        public DataTemplate AnchorableLayoutItemContentTemplate { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            if (item is SampleDocumentVm)
+                return DocumentLayoutItemContentTemplate;
+
+            if (item is SampleAnchorableVm)
+                return AnchorableLayoutItemContentTemplate;
+
+            return base.SelectTemplate(item, container);
+        }
+
+    }
+}
